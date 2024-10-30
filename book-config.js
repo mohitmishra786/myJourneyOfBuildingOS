49c1d59c874a8f30fafcde9abed58fb06824bfe6
Mohit Mishra <mohitmishra786687@gmail.com>
2024-10-30 18:48:07 +0530

    updated

--- book-config.js
+++ book-config.js
@@ -5,7 +5,7 @@ const bookConfig = {
             pages: [
                 {
                     title: "Table Of Content",
-                    path: "/myJourneyOfBuildingOS/indexOfBook.md"
+                    path: "/indexOfBook.md"
                 }
             ]
         },
@@ -14,11 +14,11 @@ const bookConfig = {
             pages: [
                 {
                     title: "Deep Dive Into Evolution And Architecture",
-                    path: "/myJourneyOfBuildingOS/Ch-1-Introduction-to-Operating-System/Deep-Dive-Into-Evolution-And-Architecture.md"
+                    path: "/Ch-1-Introduction-to-Operating-System/Deep-Dive-Into-Evolution-And-Architecture.md"
                 },
                 {
                     title: "Deep Dive Into System Calls And API Interfaces",
-                    path: "/myJourneyOfBuildingOS/Ch-1-Introduction-to-Operating-System/Deep-Dive-Into-System-Calls-And-API-Interfaces.md"
+                    path: "/Ch-1-Introduction-to-Operating-System/Deep-Dive-Into-System-Calls-And-API-Interfaces.md"
                 }
             ]
         },
@@ -27,47 +27,47 @@ const bookConfig = {
             pages: [
                 {
                     title: "Introduction to OS Architecture",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/Introduction to OS Architecture.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/Introduction to OS Architecture.md"
                 },
                 {
                     title: "Monolithic Kernel",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/Monolithic Kernel.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/Monolithic Kernel.md"
                 },
                 {
                     title: "Case Study: Traditional Unix Architecture",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/CaseStudies/Traditional Unix Architecture.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/CaseStudies/Traditional Unix Architecture.md"
                 },
                 {
                     title: "Microkernels",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/Microkernels.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/Microkernels.md"
                 },
                 {
                     title: "Case Study: Mach Kernel Architecture",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/CaseStudies/Mach Kernel Architecture.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/CaseStudies/Mach Kernel Architecture.md"
                 },
                 {
                     title: "Hybrid Kernel Architecture",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/Hybrid-Kernel-Arch.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/Hybrid-Kernel-Arch.md"
                 },
                 {
                     title: "Case Study: Windows NT Architecture",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/CaseStudies/Windows_NT.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/CaseStudies/Windows_NT.md"
                 },
                 {
                     title: "Exokernel",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/Exokernel.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/Exokernel.md"
                 },
                 {
                     title: "Unikernels",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/Unikernels.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/Unikernels.md"
                 },
                 {
                     title: "Virtual Machines as an OS Pattern",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/Virtual Machines as an OS Pattern.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/Virtual Machines as an OS Pattern.md"
                 },
                 {
                     title: "Real Time Operating Systems",
-                    path: "/myJourneyOfBuildingOS/Ch-2-OS-Architecture-Patterns/RTOS.md"
+                    path: "/Ch-2-OS-Architecture-Patterns/RTOS.md"
                 }
             ]
         }
